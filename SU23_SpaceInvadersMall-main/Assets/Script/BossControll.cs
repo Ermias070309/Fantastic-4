e452a7fbe86a9f81215d80c0240cd038db0cc000
DrDagErr <151174223+DrDagErr@users.noreply.github.com>
2024-10-21 15:18:10 +0200

    wa

--- SU23_SpaceInvadersMall-main/Assets/Script/BossControll.cs
+++ SU23_SpaceInvadersMall-main/Assets/Script/BossControll.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BossControll : MonoBehaviour
 {
+
+    Missile missilePrefab; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,16 @@ public class BossControll : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FireMissile()
+    {
+        for (int i = 0; i<10; i++)
+        {
+            float y = transform.position.y;
+            float x = transform.position.x - 5 + i;
+            Instantiate(missilePrefab, new Vector3(x, y), Quaternion.identity); 
+
+        }
     }
 }
